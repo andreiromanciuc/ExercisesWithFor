dbf8fef2fcf194ad2455eda66b27661d7a659878
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2019-12-07 17:58:56 +0200

    Exercise 4

--- src/main/java/org/fasttrackit/LogicalOp.java
+++ src/main/java/org/fasttrackit/LogicalOp.java
@@ -4,13 +4,8 @@ import java.util.Scanner;
 
 public class LogicalOp {
 
-
-    Scanner scanner = new Scanner(System.in);
-
-
-
-
     public void arrangeToMax() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your number.");
 
         for (int i =scanner.nextInt() ; i <= 100; i++) {
@@ -20,6 +15,7 @@ public class LogicalOp {
 
 
     public void arrangeToMin(){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your number.");
         for (int i =scanner.nextInt() ; i >= -100; i--) {
             System.out.println(i);
@@ -37,4 +33,21 @@ public class LogicalOp {
             System.out.println(x);
         }
     }
+
+    public void arrangeBetweenVar(){
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Please insert the numbers.");
+        int x = scanner.nextInt();
+        int y = scanner1.nextInt();
+        if(x < y){
+            for (int i = x; i <= y; i++){
+            System.out.println(i);}
+        } else {
+            for (int i = x; i >= y; i--){
+                System.out.println(i);
+            }
+        }
+    }
 }
+
