21f5d5372010229975bb2c20673491d47dd47b66
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2019-12-09 19:22:03 +0200

    Exercise 7

--- src/main/java/org/fasttrackit/LogicalOp.java
+++ src/main/java/org/fasttrackit/LogicalOp.java
@@ -8,64 +8,78 @@ public class LogicalOp {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your number.");
 
-        for (int i =scanner.nextInt() ; i <= 100; i++) {
+        for (int i = scanner.nextInt(); i <= 100; i++) {
             System.out.println(i);
         }
     }
 
 
-    public void arrangeToMin(){
+    public void arrangeToMin() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your number.");
-        for (int i =scanner.nextInt() ; i >= -100; i--) {
+        for (int i = scanner.nextInt(); i >= -100; i--) {
             System.out.println(i);
         }
     }
 
 
-    public void arrangeBetween(){
+    public void arrangeBetween() {
         Scanner scanner = new Scanner(System.in);
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Please insert the numbers.");
         int x = scanner.nextInt();
         int y = scanner1.nextInt();
-        for (; x <= y; x++){
+        for (; x <= y; x++) {
             System.out.println(x);
         }
     }
 
-    public void arrangeBetweenVar(){
+    public void arrangeBetweenVar() {
         Scanner scanner = new Scanner(System.in);
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Please insert the numbers.");
         int x = scanner.nextInt();
         int y = scanner1.nextInt();
-        if(x < y){
-            for (int i = x; i <= y; i++){
-            System.out.println(i);}
+        if (x < y) {
+            for (int i = x; i <= y; i++) {
+                System.out.println(i);
+            }
         } else {
-            for (int i = x; i >= y; i--){
+            for (int i = x; i >= y; i--) {
                 System.out.println(i);
             }
         }
     }
 
-    public void showPairNumber(){
+    public void showPairNumber() {
         int i;
-        for ( i=0; i <=100; i++ ){
-            if (i%2 ==0){
+        for (i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
                 System.out.println(i);
             }
         }
     }
 
-    public void showUnPairNumber(){
+    public void showUnPairNumber() {
         int i;
-        for (i=0; i <100; i++){
-            if (i%2 !=0){
+        for (i = 0; i < 100; i++) {
+            if (i % 2 != 0) {
                 System.out.println(i);
             }
         }
     }
+
+
+    public void sumFrom() {
+        for (int i = 60; i < 100; i++) {
+            int n = (i * (i + 1)) / 2;
+            int x = 5050;
+            int z = x - n;
+            System.out.println(z);
+            break;
+        }
+    }
+
+
 }
 
